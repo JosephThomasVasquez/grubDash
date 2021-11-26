ef5c7382490169da2f98dcff0f6a298424a8c0db
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-11-26 14:28:24 -0800

    Created orders validation middleware for POST handler

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,101 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+// MIDDLEWARE =================================================================================================
+
+const bodyHasDeliverTo = (req, res, next) => {
+  const { data: { deliverTo } = {} } = req.body;
+
+  if (deliverTo === undefined || deliverTo === "") {
+    next({
+      status: 400,
+      message: "Order must include a deliverTo",
+    });
+  }
+  next();
+};
+
+const bodyHasMobileNumber = (req, res, next) => {
+  const { data: { mobileNumber } = {} } = req.body;
+
+  if (mobileNumber === undefined || mobileNumber === "") {
+    next({
+      status: 400,
+      message: "Order must include a mobileNumber",
+    });
+  }
+  next();
+};
+
+const bodyHasDishes = (req, res, next) => {
+  const { data: { dishes } = {} } = req.body;
+
+  const dishesIsArray = Array.isArray(dishes);
+  console.log("isArray?", dishesIsArray);
+
+  if (dishes === undefined || dishes === "") {
+    return next({
+      status: 400,
+      message: "Order must include a dish",
+    });
+  }
+
+  if (!dishesIsArray || dishes.length === 0) {
+    return next({
+      status: 400,
+      message: "Order must include at least one dish",
+    });
+  }
+
+  next();
+};
+
+const bodyHasDishQuantity = (req, res, next) => {
+  const { data: { dishes } = {} } = req.body;
+
+  const filterDishes = dishes.forEach((dish, index) => {
+    const { quantity } = dish;
+
+    if (
+      quantity === undefined ||
+      quantity === "" ||
+      typeof quantity !== "number" ||
+      quantity <= 0
+    ) {
+      return next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+      });
+    }
+  });
+
+  next();
+};
+
+// ROUTE RESOURCES (CRUD HANDLERS) =============================================================================
+
+const list = (req, res) => {
+  res.json({ data: orders });
+};
+
+const read = (req, res) => {
+  res.json({ data: res.locals.order });
+};
+
+const create = (req, res) => {
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+
+  const newDish = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+};
+
+module.exports = { list };
