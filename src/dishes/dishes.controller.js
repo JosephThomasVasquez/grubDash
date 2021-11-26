d6dd17d51de683cbd7081c2d8ac23bf68d467de6
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-11-26 08:46:58 -0800

    Created dishExists validation middleware

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -8,6 +8,23 @@ const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
 
+// MIDDLEWARE =================================================================================================
+
+const dishExists = (req, res, next) => {
+  const { dishId } = req.params;
+
+  const foundDish = dishes.find((dish) => dishId === Number(dish.id));
+
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    next();
+  }
+
+  return next({ status: 404, message: `Dish id not found: ${dishId}` });
+};
+
+// ROUTE RESOURCES (CRUD HANDLERS) =============================================================================
+
 const list = (req, res) => {
   res.json({ data: dishes });
 };
